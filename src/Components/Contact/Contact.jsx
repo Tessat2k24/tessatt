3c8b8baad49110ba62706e9c2a8872d50e43306e
tessat <tessat2k24@gmail.com>
2024-07-19 07:42:14 +0530

    new

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -64,7 +64,7 @@ const Contact = () => {
             <div className="social-icons">
               <a href="#">
                 <img
-                  src="facebook.svg"
+                  src="./facebook.svg"
                   className="social-icon"
                   alt="Facebook"
                 />
