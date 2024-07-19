9defb9c3fca8061b3ad8c6e17c6d6638cdee2e2d
tessat <tessat2k24@gmail.com>
2024-07-19 09:07:04 +0530

    new

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -5,7 +5,7 @@ import "./Gallery.css";
 const images = [
   { id: 1, src: "../../assets/Image/1.jpg", alt: "Image 1" },
   { id: 2, src: "../../assets/Image/2.jpg", alt: "Image 2" },
-  { id: 3, src: "src/assets/Image/3.jpg", alt: "Image 3" },
+  { id: 3, src: "../../assets/Image/3.jpg", alt: "Image 3" },
   { id: 4, src: "src/assets/Image/4.jpg", alt: "Image 4" },
   { id: 5, src: "src/assets/Image/5.jpg", alt: "Image 5" },
   { id: 6, src: "src/assets/Image/6.jpg", alt: "Image 6" },
