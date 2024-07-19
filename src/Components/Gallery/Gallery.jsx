e4245175065d0a0041f2ba51ff9eb9c095c157de
tessat <tessat2k24@gmail.com>
2024-07-19 08:56:03 +0530

    new

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -3,8 +3,8 @@ import Masonry from "react-responsive-masonry";
 import "./Gallery.css";
 
 const images = [
-  { id: 1, src: "src/assets/Image/1.jpg", alt: "Image 1" },
-  { id: 2, src: "src/assets/Image/2.jpg", alt: "Image 2" },
+  { id: 1, src: "../assets/Image/1.jpg", alt: "Image 1" },
+  { id: 2, src: "../assets/Image/2.jpg", alt: "Image 2" },
   { id: 3, src: "src/assets/Image/3.jpg", alt: "Image 3" },
   { id: 4, src: "src/assets/Image/4.jpg", alt: "Image 4" },
   { id: 5, src: "src/assets/Image/5.jpg", alt: "Image 5" },
