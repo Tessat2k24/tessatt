4504fe4efb11e0126bec0db632b5e2bd0711ab24
Bino J Panicker <tessat2k24@gmail.com>
2024-09-16 22:32:41 +0530

    new updated

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       <div className="app">
         <About />
         <Ourteam />
-        <Program />
+        {/* <Program /> */}
         {/* <Register /> */}
         <Projects />
         <News />
